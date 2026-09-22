ece02dc5e9a56d154a8471c12602c626c97b4ff2
Wise <Tom.Wise@ons.gov.uk>
2026-09-22 09:43:05 +0100

    Material Commit

---
+++ worksheet_dataanalysis_session4.R
@@ -0,0 +1,8 @@
+# Data Analysis - Session 4 - Worksheet 
+
+# Package Loading 
+library(tidyverse) 
+
+# Reload the data in: 
+
+data <- read.csv(file = "data/welshgov_data.csv")
---
+++ worksheet_datavisualisation_session5.R
@@ -0,0 +1,8 @@
+# Data Visualisation - Session 5 - Worksheet 
+
+# Package Loading 
+library(tidyverse) 
+
+# Reload the data in: 
+
+data <- read.csv(file = "data/welshgov_data.csv")
